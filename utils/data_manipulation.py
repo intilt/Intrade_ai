7b48317c1e1abcb94b70c35582dad1f488a8aca8
Amrit Kumar Dash <95899268+kaydeeXD@users.noreply.github.com>
2022-11-30 20:26:27 +0530

    Added sup_res function in data manipulation

--- utils/data_manipulation.py
+++ utils/data_manipulation.py
@@ -1,6 +1,13 @@
 import pandas as pd
 import talib
 from talib import abstract
+import numpy as np
+from mplfinance.original_flavor import candlestick_ohlc
+import matplotlib.dates as mpl_dates
+import matplotlib.pyplot as plt
+
+plt.rcParams['figure.figsize'] = [12, 7]
+plt.rc('font', size=14)
 
 ## function to get Pivot Points, Supports and Resistances
 def PPSR(df):  
@@ -72,3 +79,71 @@ def pattern_recognition(df):
     data['dragonfly_dogi'] = abstract.CDLDRAGONFLYDOJI(data)   # type: ignore
     data['gravestone_dogi'] = abstract.CDLGRAVESTONEDOJI(data)   # type: ignore
     return data
+
+# Create two functions to calculate if a level is SUPPORT or a RESISTANCE level through fractal identification
+def is_Suppport_Level(df, i):
+    support = df['low'][i] < df['low'][i - 1] and df['low'][i] < df['low'][i + 1] and df['low'][i + 1] < df['low'][i + 2] and df['low'][i - 1] < df['low'][i - 2]
+    return support
+
+
+def is_Resistance_Level(df, i):
+    resistance = df['high'][i] > df['high'][i - 1] and df['high'][i] > df['high'][i + 1] and df['high'][i + 1] > df['high'][i + 2] and df['high'][i - 1] > df['high'][i - 2]
+    return resistance
+
+# Plotting the data
+def plot_support_resistance_levels(df, levels, level_types):
+    fig, ax = plt.subplots()
+    candlestick_ohlc(ax, df.values, width=0.6, colorup='green', colordown='red', alpha=0.8)
+    date_format = mpl_dates.DateFormatter('%d %b %Y')
+    ax.xaxis.set_major_formatter(date_format)
+    fig.autofmt_xdate()
+    fig.tight_layout()
+
+    for level, level_type in zip(levels, level_types):
+        plt.hlines(level[1],
+                xmin = df['Date'][level[0]],
+                xmax = max(df['Date']),
+                colors = 'blue')
+        plt.text(df['Date'][level[0]], level[1], (str(level_type) + ': ' + str(level[1]) + ' '), ha='right', va='center', fontweight='bold', fontsize='x-small')
+        plt.title('Support and Resistance levels', fontsize=24, fontweight='bold')
+        fig.show()
+
+# This function, given a price value, returns True or False depending on if it is too near to some previously discovered key level.
+def distance_from_mean(df, level,levels):
+    # Clean noise in data by discarding a level if it is near another
+    # (i.e. if distance to the next level is less than the average candle size for any given day - this will give a rough estimate on volatility)
+    mean = np.mean(df['high'] - df['low'])
+    return np.sum([abs(level - y) < mean for y in levels]) == 0
+
+def get_support_resistance(df):
+    df = df.set_index("datetime")
+    df['Date'] = pd.to_datetime(df.index)
+    df['Date'] = df['Date'].apply(mpl_dates.date2num)
+    df = df.loc[:,['Date', 'open', 'high', 'low', 'close']]
+    # Optimizing the analysis by adjusting the data and eliminating the noise from volatility that is causing multiple levels to show/overlapp
+    lst = []
+    levels = []
+    level_types = []
+    for i in range(2, df.shape[0] - 2):
+        if is_Suppport_Level(df, i):
+            level = df['low'][i].round(2)
+
+            if distance_from_mean(df, level,levels):
+                lst.append((df.index[i], level, 'Support'))
+                levels.append((i, level))
+                level_types.append('Support')
+
+        elif is_Resistance_Level(df, i):
+            level = df['high'][i].round(2)
+
+            if distance_from_mean(df, level,levels):
+                lst.append((df.index[i], level, 'Resistance'))
+                levels.append((i, level))
+                level_types.append('Resistance')
+    plot_support_resistance_levels(df, levels, level_types)
+    df1 = pd.DataFrame(lst)
+    df1.columns = ['datetime', 'level', 'level_type']
+    df1 = df1.set_index('datetime')
+    df2 = df.join(df1)
+    df2 = df2.drop(['Date'], axis=1)
+    return df2
