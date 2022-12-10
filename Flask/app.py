d428a8eccbbfdd24f6adb1454c02df12a4133ab9
Amrit Kumar Dash <95899268+kaydeeXD@users.noreply.github.com>
2022-12-10 21:42:50 +0530

    computing indicator, get path api, compute with s3 loc

--- Flask/app.py
+++ Flask/app.py
@@ -1,6 +1,17 @@
 from flask import Flask, render_template, request
 import pandas as pd
 import json
+import boto3
+import pandas as pd
+import talib
+
+# Creating the low level functional client
+client = boto3.client(
+    's3',
+    aws_access_key_id = 'AKIARJFZWD4TKOK4T2VO',
+    aws_secret_access_key = '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',
+    region_name = 'ap-south-1'
+)
 
 with open('config/config_app.json', 'r') as openfile:
     config_app = json.load(openfile)
@@ -11,7 +22,7 @@ app= Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/check', methods=['GET','POST'])
+'''@app.route('/check', methods=['GET','POST'])
 def check():
     if request.method=='POST':
         #getting values from the form
@@ -20,9 +31,17 @@ def check():
         date_from=request.form.get('from')
         date_to=request.form.get('to')
         
+        # Create the S3 object
+        obj = client.get_object(
+            Bucket = 'intrade-dev-data',
+            Key = config_app['instrument'][instrument]+instrument+'_'+config_app['timeframe'][timeframe]
+            )
+        
         #getting path
         path = config_app['instrument'][instrument]+instrument+'_'+config_app['timeframe'][timeframe]
         df = pd.read_csv(path)
+
+        df = pd.read_csv(obj['Body'])
 
         #as 1min dataframe doesn't have stock_code column
         try:
@@ -42,6 +61,63 @@ def check():
         df = df.loc[date_from:date_to]
         return render_template('table.html',  tables=[df.to_html(classes='data')], titles=df.columns.values)
     else:
+        return render_template('index.html')'''
+
+@app.route('/', methods=['GET'])
+def get_instruments():
+    return config_app['instrument'].items()
+@app.route('/', methods=['GET'])
+def get_timeframes():
+    return config_app['timeframe'].items()
+@app.route('/', methods=['GET'])
+def get_indicators():
+    return config_app['indicators'].items()
+@app.route('/', methods=['GET'])
+def get_indicator_params():
+    return config_app['indicator_params'].items()
+
+@app.route('/check', methods=['GET','POST'])
+def check():
+    if request.method=='POST':
+        #getting values from the form
+        instrument=request.form.get('instrument')
+        timeframe=request.form.get('timeframe')
+        date_from=request.form.get('from')
+        date_to=request.form.get('to')
+        indicator=request.form.get("indicator")
+        field=request.form.get('field')
+        timeperiod=request.form.get('timeperiod')
+        
+        # Create the S3 object
+        obj = client.get_object(
+            Bucket = 'intrade-dev-data',
+            Key = config_app['instrument'][instrument]+instrument+'_'+config_app['timeframe'][timeframe]
+            )
+
+        df = pd.read_csv(obj['Body'])
+
+        #as 1min dataframe doesn't have stock_code column
+        try:
+            df.drop(columns=['stock_code'],inplace=True)
+        except:
+            pass
+
+        #making datetime as index
+        try:
+            df['datetime'] =  pd.to_datetime(df['datetime'], infer_datetime_format=True)
+        except:
+            df['datetime']=df['date']+" "+df['time']
+            df['datetime'] =  pd.to_datetime(df['datetime'], infer_datetime_format=True)
+        df = df.set_index("datetime")
+        
+        #filtering data as per the date range
+        df = df.loc[date_from:date_to]
+        
+        #computing indicators
+        df[indicator] = getattr(talib, indicator)(df[field], timeperiod=int(timeperiod))   
+        return render_template('table.html',  tables=[df.to_html(classes='data')], titles=df.columns.values)
+    else:
         return render_template('index.html')
+
 if __name__=='__main__':
     app.run(debug=True)
